3f6a0ef01a05e0525dd887c03ce76c4e3f81140f
GisellyRock <gisellydsr@gmail.com>
2023-11-21 19:25:15 -0300

    feat:AchievementsSection

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -3,16 +3,23 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
+export async function POST(req, res) {
+  const { body } = req;
+  const { email, subject, message } = body;
+
   try {
     const data = await resend.emails.send({
-      from: 'Giselly <gisellydsr@gmail.com>',
-      to: ['gisellydsr@gmail.com'],
+      from: fromEmail,
+      to: [''],
       subject: 'Hello world',
       react: (
         <>
-          <p>Email Body</p>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
         </>
       ),
     });
